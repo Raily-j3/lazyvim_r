b601a88d291b4f2af0b32f31b3c39f840e7cb261
liruijie <liruijie@vyagoo.com>
2023-12-05 15:17:20 +0800

    use everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim", lazy = false },
+  {'sainnhe/everforest',
+    init = function()
+       vim.g["everforest_background"]= 'hard'
+    end,
+    lazy = false
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "everforest",
     },
   },
 }
